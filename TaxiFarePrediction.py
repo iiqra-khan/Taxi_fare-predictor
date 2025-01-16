1dbbea46db9a9f41043883de9e26db624309a892
Iqra Khan <iqrakhan89102@gmail.com>
2025-01-16 19:52:18 +0530

    add

--- TaxiFarePrediction.py
+++ TaxiFarePrediction.py
@@ -1,4 +1,9 @@
 import streamlit as st;
 import pandas as pd;
 import numpy as np;
-import seaborn as sns;
+# import seaborn as sns;
+
+st.write("hello world")
+
+df = pd.read_csv('taxi_trip_pricing.csv')
+df
